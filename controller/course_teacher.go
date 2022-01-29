69a03a571de220b7309cbbaa0c2c702096c07bb8
booiris <954520770@qq.com>
2022-01-29 23:31:56 +0800

    permission_check param_check match_course

--- controller/course_teacher.go
+++ controller/course_teacher.go
@@ -1,11 +1,12 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"course_selection/types"
+	"log"
+	"net/http"
 
-// 二分图匹配
-// func match_course() {
-
-// }
+	"github.com/gin-gonic/gin"
+)
 
 // 创建课程
 func Course_create(c *gin.Context) {
@@ -29,4 +30,37 @@ func Teacher_get_course(c *gin.Context) {
 
 // 二分图匹配对应课程
 func Course_schedule(c *gin.Context) {
+	var data types.ScheduleCourseRequest
+	if err := c.ShouldBind(&data); err != nil {
+		log.Println(err)
+		return
+	}
+	res := match_course(data.TeacherCourseRelationShip)
+	c.JSON(http.StatusOK, types.ScheduleCourseResponse{Code: types.OK, Data: res})
+}
+
+func match_course(data map[string][]string) map[string]string {
+	t2c := make(map[string]string)
+	c2t := make(map[string]string)
+	for k := range data {
+		if _, is_exist := t2c[k]; !is_exist {
+			check := make(map[string]struct{})
+			dfs(k, &data, &check, &t2c, &c2t)
+		}
+	}
+	return t2c
+}
+
+func dfs(now string, data *map[string][]string, check *map[string]struct{}, t2c *map[string]string, c2t *map[string]string) bool {
+	for _, i := range (*data)[now] {
+		if _, is_exist := (*check)[i]; !is_exist {
+			(*check)[i] = struct{}{}
+			if nxt, is_exist := (*c2t)[i]; !is_exist || dfs(nxt, data, check, t2c, c2t) {
+				(*c2t)[i] = now
+				(*t2c)[now] = i
+				return true
+			}
+		}
+	}
+	return false
 }
--- controller/member.go
+++ controller/member.go
@@ -1,16 +1,72 @@
 package controller
 
 import (
+	"course_selection/database"
+	"course_selection/types"
+	"log"
+	"strings"
+	"unicode"
+
 	"github.com/gin-gonic/gin"
 )
 
-// 权限检查
-// func check_permission(c *gin.Context) {
-// }
+func check_permission(c *gin.Context) bool {
+	value, err := c.Cookie("camp-session")
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	var res struct{ UserType types.UserType }
+	database.Db.Table("members").Where(&value).Find(&res)
+	return res.UserType == types.Admin
+}
 
-// 参数检查
-// func check_param(c *gin.Context) {
-// }
+func check_param(data types.CreateMemberRequest) bool {
+
+	// 用户昵称
+	len := strings.Count(data.Nickname, "")
+	if len < 4 || len > 20 {
+		return false
+	}
+
+	// 用户名
+	for _, v := range data.Username {
+		if !(unicode.IsLetter(v) || unicode.IsDigit(v)) {
+			return false
+		}
+	}
+	len = strings.Count(data.Username, "")
+	if len < 8 || len > 20 {
+		return false
+	}
+
+	// 密码
+	letter, digit := false, false
+	for _, v := range data.Password {
+		if !(unicode.IsLetter(v) || unicode.IsDigit(v)) {
+			return false
+		}
+		if unicode.IsLetter(v) {
+			letter = true
+		} else if unicode.IsDigit(v) {
+			digit = true
+		}
+	}
+	if !(letter && digit) {
+		return false
+	}
+	len = strings.Count(data.Password, "")
+	if len < 8 || len > 20 {
+		return false
+	}
+
+	// 用户类型
+	if data.UserType < 1 || data.UserType > 3 {
+		return false
+	}
+
+	return true
+}
 
 // 创建成员
 func Member_create(c *gin.Context) {
