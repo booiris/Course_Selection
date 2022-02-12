fab35102ac8af440d7323db0d3bf938c444fdb0b
booiris <954520770@qq.com>
2022-02-12 19:22:22 +0800

    update

--- test/test.go
+++ test/test.go
@@ -27,16 +27,17 @@ const (
 var student [thread + 1][]string
 var wg sync.WaitGroup
 var globalTransport *http.Transport
+var client http.Client
 
-func init() {
+func client_init() {
 	globalTransport = &http.Transport{}
-}
-
-func create_course() {
-	client := http.Client{
+	client = http.Client{
 		// Timeout:   30,
 		Transport: globalTransport,
 	}
+}
+
+func create_course() {
 	dir := "/course/create"
 	for i := 0; i < course_cnt; i++ {
 		name := "C" + strconv.Itoa(i)
@@ -55,11 +56,6 @@ func create_course() {
 
 func book_course(student_id int) {
 	defer wg.Done()
-	client := http.Client{
-		// Timeout:   30,
-		Transport: globalTransport,
-	}
-
 	// 检验过程中检查数据一致性
 	check := make(chan bool)
 	over := make(chan struct{})
@@ -95,10 +91,6 @@ func book_course(student_id int) {
 }
 
 func get_student_courses_chan(student_id int, check chan bool, over chan struct{}) {
-	client := http.Client{
-		// Timeout:   30,
-		Transport: globalTransport,
-	}
 
 	id := strconv.Itoa(student_id)
 	dir := "/student/course"
@@ -134,10 +126,6 @@ func get_student_courses_chan(student_id int, check chan bool, over chan struct{
 }
 
 func get_student_courses(student_id int) {
-	client := http.Client{
-		// Timeout:   30,
-		Transport: globalTransport,
-	}
 	id := strconv.Itoa(student_id)
 	dir := "/student/course"
 	params := "?StudentID=" + id
@@ -170,7 +158,7 @@ func get_student_courses(student_id int) {
 }
 
 func main() {
-
+	client_init()
 	create_course()
 	wg.Add(thread)
 	for i := 1; i <= thread; i++ {
