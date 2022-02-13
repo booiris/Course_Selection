4126d0e581862fa9ad44ac5173a7d519eca7a9cd
booiris <954520770@qq.com>
2022-02-13 22:17:19 +0800

    add Session

--- controller/auth.go
+++ controller/auth.go
@@ -1,10 +1,14 @@
 package controller
 
 import (
+	"context"
 	"course_selection/database"
 	"course_selection/types"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v8"
+	uuid "github.com/satori/go.uuid"
 )
 
 // 登录
@@ -20,30 +24,41 @@ func Login(c *gin.Context) {
 		c.JSON(200, types.LoginResponse{Code: types.WrongPassword})
 		return
 	}
-	c.SetCookie("camp-session", member.UserID, 3600, "/", "", false, true)
+	sessionKey := uuid.NewV4().String()
+	ctx := context.Background()
+	database.Rdb.Set(ctx, sessionKey, member.UserID, time.Second*3600)
+	c.SetCookie("camp-session", sessionKey, 3600, "/", "", false, true)
 	c.JSON(200, types.LoginResponse{Code: types.OK, Data: struct{ UserID string }{UserID: member.UserID}})
 }
 
 // 登出
 func Logout(c *gin.Context) {
-	userid, err := c.Cookie("camp-session")
+	sessionKey, err := c.Cookie("camp-session")
 	if err != nil {
 		c.JSON(200, types.LogoutResponse{Code: types.LoginRequired})
 		return
 	}
-	c.SetCookie("camp-session", userid, -1, "/", "", false, false)
+	ctx := context.Background()
+	database.Rdb.Del(ctx, sessionKey)
+	c.SetCookie("camp-session", sessionKey, -1, "/", "", false, true)
 	c.JSON(200, types.LogoutResponse{Code: types.OK})
 }
 
 // 获取成员信息
 func Whoami(c *gin.Context) {
-	userid, err := c.Cookie("camp-session")
+	sessionKey, err := c.Cookie("camp-session")
 	if err != nil {
 		c.JSON(200, types.WhoAmIResponse{Code: types.LoginRequired})
 		return
 	}
+	ctx := context.Background()
+	userid := database.Rdb.Get(ctx, sessionKey)
+	if userid.Err() == redis.Nil {
+		c.JSON(200, types.WhoAmIResponse{Code: types.LoginRequired})
+		return
+	}
 	var member types.Member
-	database.Db.Where("user_id = ?", userid).Find(&member)
+	database.Db.Where("user_id = ?", userid.Val()).Find(&member)
 	var data types.TMember
 	data.UserID = member.UserID
 	data.Nickname = member.Nickname
--- database/gorm.go
+++ database/gorm.go
@@ -1,9 +1,13 @@
 package database
 
 import (
+	"context"
+	"course_selection/types"
 	"fmt"
 	"strings"
+	"time"
 
+	"github.com/go-redis/redis/v8"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -18,16 +22,80 @@ const (
 )
 
 var Db *gorm.DB
+var Rdb *redis.Client
 
 func InitDB() {
 	//构建连接："用户名:密码@tcp(IP:端口)/数据库?charset=utf8mb4"
 	dsn := strings.Join([]string{userName, ":", password, "@tcp(", ip, ":", port, ")/", dbName, "?charset=utf8mb4&parseTime=True"}, "")
 	var err error
-	Db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	Db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{
+		PrepareStmt: true,
+	})
 
 	if err != nil {
 		fmt.Println("open database fail")
 		return
 	}
+	sqlDb, _ := Db.DB()
+	// 设置空闲连接数
+	// 数量 connections = ((core_count * 2) + effective_spindle_count)
+	sqlDb.SetConnMaxIdleTime(10)
+	// 最大连接数
+	sqlDb.SetMaxOpenConns(100)
+	// 连接复用连接时间
+	sqlDb.SetConnMaxLifetime(time.Hour)
 	fmt.Println("connnect success")
+}
+
+func InitRedis() {
+	Rdb = redis.NewClient(&redis.Options{
+		Addr:     "127.0.0.1:6379",
+		Password: "zjredis", // no password set
+		DB:       0,         // use default DB
+		PoolSize: 100,       // 连接池大小
+	})
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	_, err := Rdb.Ping(ctx).Result()
+
+	if err != nil {
+		fmt.Println("open redis fail")
+		return
+	}
+
+	// 删除 redis 缓存
+	res, err := Rdb.FlushDB(ctx).Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("delete redis:", res)
+
+	// 读取学生选课和课程列表，构建缓存
+	course_cnt := make(map[string]int)
+	var courses []struct {
+		CourseID string
+		Cap      int
+	}
+	Db.Table("courses").Find(&courses)
+	for i := range courses {
+		course_cnt[courses[i].CourseID] = courses[i].Cap
+	}
+
+	var data []types.SCourse
+	Db.Table("s_courses").Find(&data)
+	for i := range data {
+		course_cnt[data[i].CourseID] -= 1
+		err := Rdb.HSetNX(ctx, data[i].UserID, data[i].CourseID, 0).Err()
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	for k, v := range course_cnt {
+		Rdb.Set(ctx, k+"cnt", v, 0)
+	}
+
+	fmt.Println("open redis success")
 }
