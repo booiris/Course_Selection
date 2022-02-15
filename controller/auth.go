968951d775e8256c2a972dea3431dbea23859636
坞少被 <57709854+WuShaobei@users.noreply.github.com>
2022-02-15 13:15:46 +0800

    Revert "update login"
    This reverts commit 76687f1921de4eb368b3f886ab8d70ab9911d21e.

--- controller/auth.go
+++ controller/auth.go
@@ -19,11 +19,8 @@ func Login(c *gin.Context) {
 		return
 	}
 	var member types.Member
-	database.Db.Model(types.Member{}).Where("username=?", loginRequest.Username).Find(&member)
+	database.Db.Model(&types.Member{}).Where(&loginRequest).Find(&member)
 	if member == (types.Member{}) {
-		c.JSON(200, types.LoginResponse{Code: types.UserNotExisted})
-		return
-	} else if member.Password != loginRequest.Password {
 		c.JSON(200, types.LoginResponse{Code: types.WrongPassword})
 		return
 	}
